b20c898a3aa53d54f1fc7dc61bf9f82fc09787ca
PRABHAT KASHYAP <111599250+KASHYPPRABHAT254@users.noreply.github.com>
2023-03-25 10:06:17 +0530

    Create Collection.OrderedDict().py
    Task
    
    >
    
    You are the manager of a supermarket.
    
    You have a list of N items together with their prices that consumers bought on a particular day.
    
    Your task is to print each item_name and net_price in order of its first occurrence.
    
    item_name=Name of the item.
    
    net price - Quantity of the item sold multiplied by the price of each item.
    
    Input Format
    
    The first line contains the number of items, N.
    
    The next N lines contains the item's name and price, separated by a space.
    
    Constraints
    
    0 < N ≤ 100
    
    Output Format
    
    Print the item_name and net price in order of its first occurrence.
    
    Sample Input
    9
    
    BANANA FRIES 12
    
    POTATO CHIPS 30
    
    APPLE JUICE 10
    
    CANDY 5
    
    APPLE JUICE 10 CANDY 5
    
    CANDY 5
    
    CANDY 5
    
    POTATO CHIPS 30
    
    >
    
    Sample Output
    
    BANANA FRIES 12 POTATO CHIPS 60
    
    APPLE JUICE 20
    
    CANDY 20
    
    Explanation
    
    BANANA FRIES: Quantity bought: 1, Price: 12
    
    Net Price: 12
    
    POTATO CHIPS: Quantity bought: 2, Price: 30
    
    Net Price: 60
    
    APPLE JUICE: Quantity bought: 2, Price: 10
    
    Net Price: 20
    
    CANDY: Quantity bought: 4, Price: 5
    
    Net Price: 20

---
+++ Collection.OrderedDict().py
@@ -0,0 +1,21 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N = int(input())
+e  = {}
+f = {}
+for i in range(N):
+    c = input()
+    a = ''
+    b = []
+    for i in c.split():
+        if(i.isdigit()):
+            b.append(i)
+        else:
+            a = a+' '+i
+    if a in e:
+        e[a] = e[a]+1
+    else:
+        e[a] = 1
+    f[a] = int(b[0])
+for i in e:
+    print(i.strip(),int(e[i])*int(f[i]))
+
