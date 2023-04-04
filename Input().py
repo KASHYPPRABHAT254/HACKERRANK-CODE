a6bfcb175effd8e10c91dd8961c222f1078d41b5
PRABHAT KASHYAP <111599250+KASHYPPRABHAT254@users.noreply.github.com>
2023-04-04 19:33:36 +0530

    Create Input().py
    Task
    
    >
    
    You are given a polynomial P of a single indeterminate (or variable), z.
    
    You are also given the values of a and k. Your task is to verify if P(x) = k.
    
    Constraints
    
    All coefficients of polynomial P are integers.
    
    z and y are also integers.
    
    Input Format
    
    The first line contains the space separated values of x and k.
    
    The second line contains the polynomial P.
    
    Output Format
    
    Print True if P(x)=k. Otherwise, print False.
    
    Sample Input
    
    14
    
    **3 + x*2 + x + 1
    
    Sample Output
    
    True
    Explanation
    
    P(1) = 13 + 12 + 1 + 1 = 4 = k
    
    Hence, the output is True.

---
+++ Input().py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def P(x):
+    a = eval(fun)
+    return a
+x,k = map(int,input().split())
+fun = input()
+if(P(x)==k):
+    print("True")
+else:
+    print("False")
